5d4a9d944438030ebd1e8aa0ec9f2ff6df47d230
ahmadiqbalalfian-itm <ahmadiqbalalfian@gmail.com>
2026-06-27 12:12:01 +0700

    memfungsikan login admin dan menghubungkan dengan database

--- src/cikbohay/LOGIN_ADMIN.java
+++ src/cikbohay/LOGIN_ADMIN.java
@@ -175,8 +175,41 @@ public class LOGIN_ADMIN extends javax.swing.JFrame {
 
     private void btnLoginActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnLoginActionPerformed
         // TODO add your handling code here:
-        dispose();
-        new cikbohayy().setVisible(true);
+        String username = tUserAdmin.getText();
+        
+        String password = tPasswordAdmin.getText();
+        
+        if (username.length() != 0 && password.length() != 0){
+            try{
+                
+                String sql = "SELECT * FROM admin WHERE username_admin=? AND password_admin=md5(?)";
+                
+                Connection con = koneksi.konek();
+                
+                PreparedStatement ps = con.prepareStatement(sql);
+                
+                ps.setString(1, username);
+                
+                ps.setString(2, password);
+                
+                ResultSet rs = ps.executeQuery();
+                
+                if(rs.next()){
+                    
+                    dispose();
+                    new cikbohayy().setVisible(true);                    
+                } else {
+                    
+                    JOptionPane.showMessageDialog(null, "Username/password salah");
+                }
+            }catch (SQLException sQLException){
+                
+                JOptionPane.showMessageDialog(null, sQLException.getMessage());
+            }
+        } else {
+            
+            JOptionPane.showMessageDialog(null, "Username/password tidak boleh kosong");
+        }        
     }//GEN-LAST:event_btnLoginActionPerformed
 
     /**
