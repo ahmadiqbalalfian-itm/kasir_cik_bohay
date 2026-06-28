2451163734ed77152f3eec94a6a8c5304db571e0
ahmadiqbalalfian-itm <ahmadiqbalalfian@gmail.com>
2026-06-28 12:20:22 +0700

    method pengisian data ke tabel menu mie

--- src/cikbohay/tampilanMenu.java
+++ src/cikbohay/tampilanMenu.java
@@ -29,6 +29,7 @@ public class tampilanMenu extends javax.swing.JFrame {
         load_tabel_menu_seblak();
         load_tabel_menu_geprek();
         load_tabel_menu_minuman();
+        load_tabel_menu_mie();
     }
 
     private void pindahKartuMenu(String namaKartuMenu) {
@@ -272,6 +273,47 @@ public class tampilanMenu extends javax.swing.JFrame {
         pindahKartuMenu("cardMenuMie");//menamppilkan menu Mie LVL
     }//GEN-LAST:event_btnMenuMieActionPerformed
 
+            void load_tabel_menu_mie() {
+
+        DefaultTableModel model = new DefaultTableModel();
+
+        model.addColumn("ID MENU");
+
+        model.addColumn("Nama Menu");
+        
+        model.addColumn("Harga");
+
+        model.addColumn("Kategori");
+
+        String sql = "SELECT * FROM menu WHERE id_kategori = 'k001'";
+
+        try {
+            Connection con = koneksi.konek();
+
+            Statement st = con.createStatement();
+
+            ResultSet rs = st.executeQuery(sql);
+
+            while (rs.next()) {
+
+                String idMenu = rs.getString("id_menu");
+
+                String namaMenu = rs.getString("nama_menu");
+                
+                String harga = rs.getString("harga");
+
+                String idKategori = rs.getString("id_kategori");
+
+                Object[] baris = {idMenu, namaMenu, harga, idKategori};
+                model.addRow(baris);
+            }
+
+        } catch (SQLException sQLException) {
+            JOptionPane.showMessageDialog(null, "Gagal mengambil data!");
+        }
+        tblMenuMie.setModel(model);
+    }
+            
     private void btnMenuSemuaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnMenuSemuaActionPerformed
         // TODO add your handling code here:
         pindahKartuMenu("cardMenuSemua");//menamppilkan semua menu
