35f10b3c0fc4ca6ef843474be9a37429c14ba691
ahmadiqbalalfian-itm <ahmadiqbalalfian@gmail.com>
2026-06-28 13:55:11 +0700

    load tabel snack untuk menampilkan data snack

--- src/cikbohay/tampilanMenu.java
+++ src/cikbohay/tampilanMenu.java
@@ -24,7 +24,6 @@ public class tampilanMenu extends javax.swing.JFrame {
      */
     public tampilanMenu() {
         initComponents();
-
         load_tabel_menu_semua();
         load_tabel_menu_seblak();
         load_tabel_menu_geprek();
@@ -319,7 +318,7 @@ public class tampilanMenu extends javax.swing.JFrame {
         pindahKartuMenu("cardMenuSemua");//menamppilkan semua menu
     }//GEN-LAST:event_btnMenuSemuaActionPerformed
 
-    void load_tabel_menu_semua() {
+    public void load_tabel_menu_semua() {
 
         DefaultTableModel model = new DefaultTableModel();
 
@@ -503,6 +502,46 @@ public class tampilanMenu extends javax.swing.JFrame {
         pindahKartuMenu("cardMenuSnack");//menamppilkan menu Snack
     }//GEN-LAST:event_btnMenuSnackActionPerformed
 
+        void load_tabel_menu_snack() {
+
+        DefaultTableModel model = new DefaultTableModel();
+
+        model.addColumn("ID MENU");
+
+        model.addColumn("Nama Menu");
+
+        model.addColumn("Harga");
+
+        model.addColumn("Kategori");
+
+        String sql = "SELECT * FROM menu WHERE id_kategori = 'k003'";
+
+        try {
+            Connection con = koneksi.konek();
+
+            Statement st = con.createStatement();
+
+            ResultSet rs = st.executeQuery(sql);
+
+            while (rs.next()) {
+
+                String idMenu = rs.getString("id_menu");
+
+                String namaMenu = rs.getString("nama_menu");
+
+                String harga = rs.getString("harga");
+
+                String idKategori = rs.getString("id_kategori");
+
+                Object[] baris = {idMenu, namaMenu, harga, idKategori};
+                model.addRow(baris);
+            }
+
+        } catch (SQLException sQLException) {
+            JOptionPane.showMessageDialog(null, "Gagal mengambil data!");
+        }
+        tblMenuSnack.setModel(model);
+    }
     /**
      * @param args the command line arguments
      */
